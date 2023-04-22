2bdc699bda56ea594b80efd06f0b0f324cd03edf
Priyasha <you@example.com>
2023-04-22 18:08:12 +0530

    Intial-setup

---
+++ App.js
@@ -0,0 +1,13 @@
+ import React from "react";
+ import ReactDOM from "react-dom/client";
+
+const MyApp = () => (
+  <div>
+ <h1>Carrers</h1>
+   
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<MyApp/>);
+
+
